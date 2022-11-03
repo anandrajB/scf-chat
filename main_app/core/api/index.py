4d58cad9d179634cefbc95c07b450377a844cc6f
anandrajB <anand98.ar@gmail.com>
2022-11-03 17:42:02 +0530

    updated core

--- main_app/core/api/index.py
+++ main_app/core/api/index.py
@@ -3,6 +3,16 @@ from flask import render_template, Blueprint, request
 index_bp = Blueprint('index_bp', __name__, url_prefix='/')
 
 
+
+# HOME PAGE 
+
 @index_bp.route('/')
 def index():
     return render_template('index.html')
+
+
+# PROJECT AND API DESCRIPTION PAGE 
+
+@index_bp.route('/description')
+def description():
+    return render_template('welcome.html')
